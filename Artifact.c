7183c1186e7b6869b975595f18065087d5778aa8
Chloe Capriotti <c.s.capriotti@gmail.com>
2025-06-30 14:05:39 -0400

    added comments, clarified code and variable names

--- Artifact.c
+++ Artifact.c
@@ -4,52 +4,53 @@
 #include <string.h>
 #include <math.h>
 
+/* constant that Initalizes variables */
 #define INITALIZE 0
-
+/* loop variables for reseting and breaking loops */
 #define LOOP_RESET 0
 #define LOOP_BREAK 1
-
+/* the max and min for the map */
 #define MAX_SIZE 40
 #define MIN_SIZE 2
-
+/* array checkers, numeral values that represent certain events in the array */
 #define EMPTY_SPOT -1
 #define FOUND_TREASURE -2
 #define OUT_OF_RANGE -3
-
+/* menu instance: changes the output of the map depending on user choice/first instance */
 #define FIRST_LOAD 0
 #define DEBUG 1
 #define LOAD_PLAY 2
-
+#define EXISTS 1
 int main(){
-    int mapStatus = FIRST_LOAD;
-    int height = INITALIZE;
-    int width = INITALIZE;
+    /* game state control*/
+    int mapStatus = FIRST_LOAD; /* controls map display mode*/
+    int loopCondition = LOOP_RESET; /* controls exit of main game loop*/
+    int menuChoice = INITALIZE;
+    /*map dimesnsions*/
+    int height = INITALIZE; int width = INITALIZE;
+    /*artifact variables*/
     int maxArtifacts = INITALIZE;
     int numOfArtifacts = INITALIZE;
-    int i = INITALIZE; 
-    int j = INITALIZE; 
-    int loopCondition = LOOP_RESET;
-    int menuChoice = INITALIZE;
-    int userX = INITALIZE; 
-    int userY = INITALIZE;
-    int pointCounter = INITALIZE;
-    int pointsEarned = INITALIZE;
+    int foundArtifacts = INITALIZE;
+    int pointCounter = INITALIZE; /* counts total points */ int points = INITALIZE; /* amount of points (1 or 2)*/
+    /*loop and temporary usage*/
+    int i = INITALIZE; int j = INITALIZE; 
     int artifactIndex = INITALIZE;
-    int foundAll = INITALIZE;
-    int min = INITALIZE;
-    int exists = INITALIZE;
-    int *artifactx;
-    int *artifacty;
+    int closestArtifact = INITALIZE; /* stores minimum distance to the nearest artifact*/
+    int codeInDatabase = INITALIZE;
+    /* user input for searching*/
+    int userX = INITALIZE; int userY = INITALIZE;
+    /*artifact placement*/
+    int *artifactx; int *artifacty;
     int *distance;
-    char *foundCode;
-    int **arr;
-    int **digSite;
-    char *artifactCode;
+    /* game maps*/
+    int **internalDigsite; int **digSite;
+    /*code management - */
     char **artifactCodeIndex;
-    char *input;
-    
-    input = malloc(100 * sizeof(int *));
+    char *foundCode; char *artifactCode;
+    char *input; /* input buffer */
 
+    input = malloc(100 * sizeof(int *));
     /*ask user for height and width of map*/
     while (!loopCondition){
         printf("Enter map height (2 - 40): "); 
@@ -65,7 +66,6 @@ int main(){
             printf("Error: height out of bounds - enter an integer between %d - %d\n", MIN_SIZE, MAX_SIZE);
             continue;
         }
-        
 
         printf("Enter map width (2 - 40): "); 
         while (fgets(input, 100, stdin)){
@@ -80,19 +80,19 @@ int main(){
             printf("Error: width out of bounds - enter an integer between %d - %d\n", MIN_SIZE, MAX_SIZE);
             continue;
         }
-        loopCondition = LOOP_BREAK; /*exit loop*/
+        loopCondition = LOOP_BREAK;
     }
 
 
-    /*creates a 2d array based on height and width*/
-    arr = malloc(height * sizeof(int *));
+    /*creates a 2d array based on height and width - stores where artifacts are placed and their index*/
+    internalDigsite = malloc(height * sizeof(int *));
     for (i = 0; i < height; i++) {
-        *(arr + i) = malloc(width * sizeof(int));
+        *(internalDigsite + i) = malloc(width * sizeof(int));
         for (j = 0; j < width; j++) {
-        *(*(arr + i) + j) = EMPTY_SPOT;
+        *(*(internalDigsite + i) + j) = EMPTY_SPOT;
         }
     }
-
+    /*user dig history*/
     digSite = malloc(height * sizeof(int *));
     for (i = 0; i < height; i++){
         *(digSite + i) = malloc(width * sizeof(int));
@@ -100,10 +100,11 @@ int main(){
             *(*(digSite + i) + j) = EMPTY_SPOT;  /* initially nothing dug*/
         }
     }
-
+    /*creates 2 arrays for x, y of artifacts - used for determining how far the closest artifact is compared to a users guess*/
     artifactx = malloc(numOfArtifacts * sizeof(int)); artifacty = malloc(numOfArtifacts * sizeof(int));
     
-    /*creates the artifact amount*/
+
+    /*creates the artifact amount via user input*/
     maxArtifacts = (height * width); /*upper bound is height * width*/
     loopCondition = LOOP_RESET;
     while(!loopCondition){
@@ -132,9 +133,9 @@ int main(){
         *(artifactCodeIndex + i) = malloc(5 * sizeof(char));
         loopCondition = LOOP_RESET;
         while(!loopCondition){
-            printf("Code for atrifact %d: ", i + 1); 
+            printf("Code for artifact %d: ", i + 1); 
             scanf("%4s", artifactCode);
-
+            /*uses ctype library; parses the artifact code into unsigned chars to check if they follow the correct order of types including EOF /0 (the reason it is size 5 and not 4)*/
             if (!isalpha((unsigned char) * (artifactCode)) || !isdigit((unsigned char) * (artifactCode + 1)) || !isdigit((unsigned char) * (artifactCode + 2)) || !isdigit((unsigned char) * (artifactCode + 3)) || ((unsigned char) * (artifactCode + 4)) != '\0') {
             printf("Error: input is in incorrect format, re-enter with a valid code (ex. S743, y824)\n");
             continue;
@@ -142,29 +143,30 @@ int main(){
             loopCondition = LOOP_BREAK;
         }
 
-        /*FIX THIS BRUVV */
+        /*checks if an artifact code is already in the artifact code index */
         for (j = 0; j < i; j++) {
-            if (strcmp(artifactCode, *(artifactCodeIndex + j)) == 0) {
-                exists = 1;
+            if (strcmp(artifactCode, *(artifactCodeIndex + j)) == 0) { /*uses string compare to compare the entered code with each code in the index and flags duplicatesssssss*/
+                codeInDatabase = EXISTS;
                 break;
             }
         }
-        if(exists){
+        /*asks the user again if the code is invalid instead of copying it to the index*/
+        if(codeInDatabase){
             printf("Error: code already exists in database, re-enter a different valid code (ex. S743, y824)\n");
             continue;
         }
-        strcpy(*(artifactCodeIndex + i), artifactCode);
+        strcpy(*(artifactCodeIndex + i), artifactCode); /*string copy to copy the code into the index*/
         break;
         }
     }
     
 
-    /*put artifacts into random spot in the 2d array*/
+    /*put artifacts into random spot in the 2d array - checks if the spot if empty*/
     for (i = 0; i < numOfArtifacts; i++){
         *(artifacty + i) = rand() % (height); *(artifactx + i) = rand() % (width); /* random % (max - min + 1) + min | min = 0 */
-        if (*(*(arr + *(artifacty + i)) + *(artifactx + i)) == EMPTY_SPOT) {
-            *(*(arr + *(artifacty + i)) + *(artifactx + i)) = i;
-        } else {
+        if (*(*(internalDigsite + *(artifacty + i)) + *(artifactx + i)) == EMPTY_SPOT) { /* if the random spot is empty, this is where the artifact goes*/
+            *(*(internalDigsite + *(artifacty + i)) + *(artifactx + i)) = i;
+        } else { /* if the random spot already has an artifact, retry*/
             i--;
         }
     }
@@ -172,6 +174,7 @@ int main(){
 
     /*Print the map - without anything extra*/
     loopCondition = LOOP_RESET;
+    /*prints the map header - this never changes so yeah*/
     while(!loopCondition){
         printf("Map:\n\t");
         for (i = 0; i < width; i++) {
@@ -186,11 +189,11 @@ int main(){
             printf("-");
         }
         printf("\n");
-        
+        /*on first load just print empty spaces for map :D*/
         if(mapStatus == FIRST_LOAD){
           for (i = 0; i < height; i++) {
             if (i < 10){
-                printf("     0%d|", i);
+                printf("     0%d|", i); /*if the number is les than 10, adds a 0 for better readability*/
             } else {
                 printf("     %2d|", i);
             }
@@ -199,36 +202,19 @@ int main(){
             }
             printf("\n");
             }  
-        }
-        if (mapStatus == LOAD_PLAY){
-            for (i = 0; i < height; i++){
-                printf("     %2d|", i);
-                for(j=0; j < width; j++){
-                    if(*(*(digSite + i) + j) == FOUND_TREASURE){
-                        printf("!");
-                    } else if (*(*(digSite + i) + j) >= 1 && *(*(digSite + i) + j) <= 9){
-                        printf("%d", *(*(digSite + i) + j));
-                    } else if (*(*(digSite + i) + j) == OUT_OF_RANGE){
-                        printf("#");
-                    } else {
-                        printf(" ");
-                    }
-                }
-                printf("\n");
-            }
-        }
-        if (mapStatus == DEBUG){
+        } else {
             for (i = 0; i < height; i++) {
                 printf("     %2d|", i);
                 for (j = 0; j < width; j++) {
-                    if (*(*(digSite + i) + j) == FOUND_TREASURE){
-                        printf("!");
-                    } else if (*(*(digSite + i) + j) >= 1 && (*(*(digSite + i) + j) <= 9)) {
-                        printf("%d",*(*(digSite + i) + j));
-                    } else if (*(*(digSite + i) + j) == OUT_OF_RANGE){
-                        printf("#");
-                    } else if (*(*(arr + i) + j) != EMPTY_SPOT && (*(*(arr + i) + j) != FOUND_TREASURE)){ /*if empty and not user point*/
+                    /*if user is in debug mode, print map WITH hidden artifacts*/
+                    if (mapStatus == DEBUG && (*(*(internalDigsite + i) + j) != EMPTY_SPOT) && (*(*(internalDigsite + i) + j) != FOUND_TREASURE) && (*(*(digSite + i) + j) == EMPTY_SPOT)){ /*if not empty(treasure) and unfound*/ 
                         printf("?");
+                    } else if (*(*(digSite + i) + j) == FOUND_TREASURE){
+                        printf("!");
+                    } else if (*(*(digSite + i) + j) >= 1 && (*(*(digSite + i) + j) <= 9)) { /*if the dig spot is a number between 1-9 print(this is the closest aritfact)*/
+                        printf("%d",*(*(digSite + i) + j));
+                    } else if (*(*(digSite + i) + j) == OUT_OF_RANGE){ /*if the user chooses a spot that is beyond 9 spaces*/
+                        printf("#");
                     } else {
                         printf(" ");
                     }
@@ -236,29 +222,30 @@ int main(){
                 printf("\n");
             }
         }
+
         
-        /*if all variables are found display a message and exit - maybe a found variables variable and when it equals the numOfVariable*/
-        foundAll = 0;
+        /*if all variables are found display a message and exit*/
         for (i = 0; i < height; i++){
             for (j = 0; j < width; j++){
-                if (*(*(arr + i) + j) == FOUND_TREASURE){
-                    foundAll ++;
+                if (*(*(internalDigsite + i) + j) == FOUND_TREASURE){
+                    foundArtifacts ++;
                 }
             }  
         }
-        if (foundAll == numOfArtifacts){
+        /*if the amount of found artifacts is equal to all artifacts - you found all of them yay*/
+        if (foundArtifacts == numOfArtifacts){
             printf("\nCongratulations! You have found all artifacts.\nTotal points: %d\n", pointCounter);
             loopCondition = LOOP_BREAK; /*exit main loop*/
             continue;
         }
-
-        menuChoice = 0;
+        /* main switch menu logic*/
         printf("1: Dig at a spot\n2: Exit\n3: Activate debug mode\n -> ");
         scanf("%d", &menuChoice);
-        while (getchar() != '\n');
+        while (getchar() != '\n'); /*consumes newline - does a little error without it*/
         switch(menuChoice){
             case 1:
-                mapStatus = LOAD_PLAY;
+                mapStatus = LOAD_PLAY; /*makes mapStatus not equal debug or first load*/
+                /*user x and y inputs - check if they are within the bounds of the map*/
                 printf("x = "); 
                     while (fgets(input, 100, stdin)){
                         if (sscanf(input," %d", &userX) == 1){
@@ -275,61 +262,62 @@ int main(){
                         printf("Error: y input is in incorrect format, re-enter with an integer value\ny = ");
                     }
                 }
-                
                 if (userX < 0 || userX >= height || userY < 0 || userY >= width){
                     printf("Error: coordinates out of bounds\n");
                     continue;
                 }
                 
-                artifactIndex = *(*(arr + userY) + userX);
+
+                /*extra cases based on user x and y - found artifact(how many points? - checks if code starts with a upper or lower)*/
+                artifactIndex = *(*(internalDigsite + userY) + userX);
                 if (artifactIndex != EMPTY_SPOT && artifactIndex != FOUND_TREASURE){
                     foundCode = *(artifactCodeIndex + artifactIndex);
-                    pointsEarned = isupper((unsigned char) * (foundCode)) ? 2 : 1;
-                    printf("You have found an artifact! (Artifact code: %s, %d point%s)\n", foundCode, pointsEarned, pointsEarned > 1 ? "s" : "");
-                    pointCounter += pointsEarned;
-                    *(*(arr + userY) + userX) = FOUND_TREASURE;
-                    *(*(digSite + userY) + userX) = FOUND_TREASURE;
-                } else if (*(*(digSite + userY) + userX) == FOUND_TREASURE){
+                    points = isupper((unsigned char) * (foundCode)) ? 2 : 1; /*parses foundCode to unsigned char; is it upper? if yes points = 2; if no points = 1*/
+                    printf("You have found an artifact! (Artifact code: %s, %d point(s))\n", foundCode, points);
+                    pointCounter += points; /*add point counter by the amount of points*/
+                    *(*(internalDigsite + userY) + userX) = FOUND_TREASURE; *(*(digSite + userY) + userX) = FOUND_TREASURE; /*flags this spot as a found artifact*/
+                } else if (*(*(digSite + userY) + userX) == FOUND_TREASURE){ /*checks if the artifact was already found*/
                     printf("You have already found this artifact\n");
-                } else if (*(*(digSite + userY) + userX) != EMPTY_SPOT){
+                } else if (*(*(digSite + userY) + userX) != EMPTY_SPOT){ /*checks if the spot was already dug(not an artifact)*/
                     printf("You already dug here\n");
                 } else {
+                    /*calculates the distance between user coord and closest artifact*/
                     distance = malloc(numOfArtifacts * sizeof(int));
                     for (i = 0; i < numOfArtifacts; i++){
                         *(distance + i) = (int)(sqrt((userX - *(artifactx + i)) * (userX - *(artifactx + i)) + (userY - *(artifacty + i)) * (userY - *(artifacty + i))));   
                     }
-                    min = 100;
+                    closestArtifact = *(distance); /*makes closest artifact the first index of distance array*/
                     for (i=0; i < numOfArtifacts; i++){
-                        if (*(distance + i) < min){
-                            min = *(distance + i);
+                        if (*(distance + i) < closestArtifact){ /*if index i is less than closestArtifact - replace it with index i*/
+                            closestArtifact = *(distance + i);
                         }
                     }
                     free(distance);
-                    if (min <= 9){
-                        *(*(arr + userY) + userX) = min;
-                        *(*(digSite + userY) + userX) = min;
-                    } else if (min > 9) {
-                        *(*(arr + userY) + userX) = OUT_OF_RANGE;
+                    if (closestArtifact <= 9){ /*if the closest artifact is within the 1-9 range, records the coordinate in the digsite arrays*/
+                        *(*(internalDigsite + userY) + userX) = closestArtifact;
+                        *(*(digSite + userY) + userX) = closestArtifact;
+                    } else if (closestArtifact > 9) { /*if the closest is larger than 9 - becomes out of range*/
+                        *(*(internalDigsite + userY) + userX) = OUT_OF_RANGE;
                         *(*(digSite + userY) + userX) = OUT_OF_RANGE;
                     }
                 }
-                
                 continue;
             case 2:
-                loopCondition = LOOP_BREAK;
+                loopCondition = LOOP_BREAK; /*breaks out of big loop and goes to freeing memory*/
                 break;
             case 3:
                 printf("Debug mode activated \n\n");
-                mapStatus = DEBUG;
+                mapStatus = DEBUG; /*changes mapStatus to debug mode - when looping back it will include debug stuff*/
                 continue;
         }
     } 
+    /*free the memory from its cages*/
     for (i = 0; i < height; i++) {
-        free(*(arr + i));
+        free(*(internalDigsite + i));
         free(*(digSite + i));
     }
     free(digSite);
-    free(arr);
+    free(internalDigsite);
     for (i = 0; i < numOfArtifacts; i++) {
         free(*(artifactCodeIndex + i));
     }  
